4fa6081b952419200f931a866f2ec7ff6cb67941
gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2022-10-15 17:38:16 -0300

    finalizando layout

--- src/App.tsx
+++ src/App.tsx
@@ -44,6 +44,15 @@ export function App() {
 
           <Button className='mt-4' type="submit" size='sm'>Entrar na plataforma</Button>
         </form>
+
+        <footer className="flex flex-col items-center gap-4 mt-8">
+          <Text asChild size='sm'>
+            <a href="" className='text-gray-400 hover:underline hover:text-gray-200'>Esqueceu sua senha ?</a>
+          </Text>
+          <Text asChild size='sm'>
+            <a href="" className='text-gray-400 hover:underline hover:text-gray-200'>Não possui conta? Crie uma agora!</a>
+          </Text>
+        </footer>
       </div>
     </>
   )
